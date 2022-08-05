6fcf425ddebefe5191ce4fd145fed127cd86cffb
loganwong52 <loganwong52@gmail.com>
2022-08-04 17:41:50 -0700

    disabled responseive grid and added eventlistener for arrow keys, and displays the location of the player

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,110 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const ROWMAX = 8
+  const COLMAX = 15
 
-  let box = <div className='box'>
-    hello
-  </div>
+  const [playerRow, setPlayerRow] = useState(7)
+  const [playerCol, setPlayerCol] = useState(7)
+
+  // prevent the arrow keys from scrolling the page!!!
+  window.addEventListener("keydown", function (e) {
+    if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.code) > -1) {
+      e.preventDefault();
+    }
+  }, false);
+
+  //detecting arrow key presses
+  function keypress(e) {
+    switch (e.keyCode) {
+      case 37:
+        if (playerCol > 0) {
+          console.log('left', playerCol - 1);
+          setPlayerCol(playerCol - 1)
+
+        } else {
+          alert("You can't go further left!")
+        }
+        break;
+      case 38:
+        if (playerRow > 0) {
+          console.log('up', playerRow - 1);
+          setPlayerRow(playerRow - 1)
+
+        } else {
+          alert("You can't go further up!")
+        }
+        break;
+      case 39:
+        if (playerCol < COLMAX - 1) {
+          console.log('right', playerCol + 1);
+          setPlayerCol(playerCol + 1)
+
+        } else {
+          alert("You can't go further right!")
+        }
+        break;
+      case 40:
+        if (playerRow < ROWMAX - 1) {
+          console.log('down', playerRow + 1);
+          setPlayerRow(playerRow + 1)
+
+        } else {
+          alert("You can't go further down!")
+        }
+        break;
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', keypress)
+    return () => document.removeEventListener("keydown", keypress);
+  }, [playerRow, playerCol])
+
+
+
+
+  // CODE FOR FILLING UP THE GRID
+  const boxStyle = {
+    backgroundColor: 'purple',
+  }
 
   const renderGrid = () => {
     let grid = []
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < ROWMAX; i++) {
       grid.push([])
-      for (let j = 0; j < 15; j++) {
+      for (let j = 0; j < COLMAX; j++) {
         grid[grid.length - 1].push(
-          <div className='box'>
+          <div className='box' style={boxStyle}>
             {`${i}, ${j}`}
-          </div>
+          </div >
         )
       }
-      // grid.push(<br />)
     }
-
 
     return (
       <div className='grid'>
         {grid}
       </div>
     )
-
   }
+
+  // useEffect(() => {
+  //   if (playerRow) {
+
+  //   }
+
+  // }, [playerRow, playerCol])
+
 
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <h1>ROW: {playerRow} | COL: {playerCol}</h1>
 
       {renderGrid()}
+
     </div>
   )
 }
