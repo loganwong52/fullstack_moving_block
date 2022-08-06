50abf52586c23d6e06a70b1dcd30d02f1bc6b39c
loganwong52 <loganwong52@gmail.com>
2022-08-06 00:24:20 -0700

    added code to backend django to set requests to Noun api to get images, and renders images in HomePage

--- block_proj/block_app/urls.py
+++ block_proj/block_app/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.send_the_homepage)
+    path('', views.send_the_homepage),
+    path('wish/<int:id>/', views.wish)
 ]
--- block_proj/block_app/views.py
+++ block_proj/block_app/views.py
@@ -2,7 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 
+import requests
+from requests_oauthlib import OAuth1
+import pprint
+import os
+from dotenv import load_dotenv
+
+load_dotenv()   # this func takes the variables from our .env file and adds them to os.environ
+print(os.environ)
+pp = pprint.PrettyPrinter(indent=2, depth=2)
+
 def send_the_homepage(request):
     print('home!')
     theIndex = open('static/index.html').read()
     return HttpResponse(theIndex)
+
+
+def wish(request, id):
+    # this Noun API requires authentication... a public and private key
+    auth = OAuth1(os.environ['apikey'], os.environ['secretkey'])
+
+    # this is the route
+    endpoint = f"http://api.thenounproject.com/icon/{id}"
+
+    # you're now passing 2 args to .get(). The route, as well as the auth, since 
+    # this API database is more protected, we need to use backend to access it
+    response = requests.get(endpoint, auth=auth)
+    responseJSON = response.json()
+    # pp.pprint(responseJSON)
+    
+    icon_url = responseJSON['icon']['preview_url']
+    # print(icon_url)
+    
+    return HttpResponse(icon_url)
