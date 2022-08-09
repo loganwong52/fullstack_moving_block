d2240982a028f0cbacaf58267c68dd4954ecc7c6
loganwong52 <loganwong52@gmail.com>
2022-08-09 00:45:11 -0700

    added previous and next buttons that display certain top 10 scores

--- frontend/src/pages/ScoresPage.jsx
+++ frontend/src/pages/ScoresPage.jsx
@@ -4,28 +4,40 @@ import { Link } from 'react-router-dom'
 
 function ScorePage() {
 
+    const [allUsers, setUsers] = useState([])
     const [scores, setScores] = useState([])
+    const [startAt, setStartAt] = useState(0)
+    const [showPrevButton, setShowPrevButton] = useState(false)
+    const [showNextButton, setShowNextbutton] = useState(true)
+
+    // helper function that fills scores array
+    function populateScoresArr() {
+        let elements = []
+        let user = null
+        let stop = startAt + 10
+        if (stop > allUsers.length) {
+            stop = allUsers.length
+        }
+
+        for (let i = startAt; i < stop; i++) {
+            user = allUsers[i]
+            elements.push(
+                <h3>
+                    {i + 1}. name: {user.name}
+                    <br />
+                    score: {user.score}
+                </h3>
+            )
+        }
+        setScores(elements)
+    }
+
 
     // on Mount, render the scores
     useEffect(() => {
-        let elements = []
-
         axios.get('/getScores').then((response) => {
             let allUsers = response.data.data
-            // console.log('response from getScores ', allUsers)
-
-            let user = null
-            for (let i = 1; i <= allUsers.length; i++) {
-                user = allUsers[i - 1]
-                elements.push(
-                    <h3>
-                        {i}. name: {user.name}
-                        <br />
-                        score: {user.score}
-                    </h3>
-                )
-            }
-            setScores(elements)
+            setUsers(allUsers)
 
         }).catch((error) => {
             console.log("There was an error while showing the TOP scores!", error)
@@ -33,6 +45,48 @@ function ScorePage() {
 
     }, [])
 
+
+    // When allUsers is populated, get the top 10 scores
+    useEffect(() => {
+        if (allUsers.length > 0)
+            populateScoresArr()
+    }, [allUsers])
+
+
+    // Handle when the previous button is clicked
+    const handlePrevClick = () => {
+        console.log("go back to previous 10")
+        setStartAt(startAt - 10)
+    }
+
+    // Handle when the next button is clicked
+    const handleNextClick = () => {
+        console.log("go to next 10")
+        setStartAt(startAt + 10)
+    }
+
+
+    // When either previous or next is clicked, 
+    // update whether to render the buttons or not
+    // and then change the top 10 scores that are rendered
+    useEffect(() => {
+        if (startAt > 0) {
+            setShowPrevButton(true)
+        } else {
+            setShowPrevButton(false)
+        }
+
+        console.log(allUsers.length)
+        if (allUsers.length > startAt + 10) {
+            setShowNextbutton(true)
+        } else {
+            setShowNextbutton(false)
+        }
+
+        if (allUsers.length > 0)
+            populateScoresArr()
+
+    }, [allUsers, startAt])
 
 
     return (
@@ -43,11 +97,22 @@ function ScorePage() {
             {/* Dispay top 10 scores */}
             {scores}
 
-            <button>
+            {/* Button that shows the previous 10 scores */}
+            {
+                showPrevButton &&
+                <button onClick={() => { handlePrevClick() }}>Show Previous 10</button>
+            }
+
+            <button className="goHomeBtn">
                 <Link to={'/'}>Return to Home Page</Link>
             </button>
 
-        </div>
+            {/* Button that shows the next 10 scores */}
+            {
+                showNextButton &&
+                <button onClick={() => { handleNextClick() }}>Show Next 10</button>
+            }
+        </div >
     )
 }
 
